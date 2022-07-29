07de19c4d3c98b1c3d540ab32ddd6e22b92c9b81
pratikgithub123 <dangipratik19@gmail.com>
2022-07-29 11:33:00 +0545

    dsa coursework

---
+++ Week2/week2.java
@@ -0,0 +1,11 @@
+// you are given an array which includes product of subset of array any array 'S' of length l, 
+// Find element of an array S 
+// of length L.
+// l=3, a [] = {10,10,5,0,2,1,2,5}
+// a [] = {2,1,5} will be able to output provided subset product.
+
+package Week2;
+
+public class week2 {
+    
+}
---
+++ Week6/week6.java
@@ -0,0 +1,12 @@
+// you are provided with an array of different words and target words. each character of 
+// word represents different digit from 0 to 9 and each 
+// different characters must represent different digits. return if sum of number represented 
+// on array equals to the number 
+// represented by targeted word, otherwise return false.
+
+
+package Week6;
+
+public class week6 {
+    
+}
